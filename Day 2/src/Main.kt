6eca485dac0760088fa09c8fa37a86165efa4b6e
Foodie <olaorefouad565@gmail.com>
2019-10-08 16:22:40 +0100

    day 2: dived into data types!

---
+++ Day 2/src/Main.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+    var i = 12
+    var d = 4.0
+    var str = "Hackerrank "
+
+    println("Welcome to the Day 2 Challenge!")
+    i += readLine()!!.toInt()
+    d += readLine()!!.toDouble()
+
+    println("$i\n$d\n$str${ readLine()!! }")
+}
